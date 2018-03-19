93a13586cacb13dfc23ed97e8ded3b2a1a428fb8
Fremy Santana <frmysantana@yahoo.com>
2018-03-18 22:14:35 -0400

    Added tests for ExpenseForm and AddExpensePage. Explored test spies and the mapDispatchToProps argument for the Redux connect function.

--- React/Expensify-app/src/Tests/Components/ExpenseForm.test.js
+++ React/Expensify-app/src/Tests/Components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../Components/ExpenseForm';
 import expenses from '../Fixtures/expenses';
+import moment from 'moment';
 
 test('Should render ExepnseForm correctly', () => {
   const wrapper = shallow(<ExpenseForm />);
@@ -59,8 +60,30 @@ test('Should not set amount if input is invalid', () => {
   expect(wrapper.state('amount')).toBe('');
 });
 
-// test('Should call onSubmit prop for valild form submission', () => {
-//   const onSubmitSpy = jest.fn();
-//   onSubmitSpy('Andrew', 'Philadelphia');
-//   expect(onSubmitSpy).toHaveBeenCalledWih();
-// });
+test('Should call onSubmit prop for valild form submission', () => {
+  const onSubmitSpy = jest.fn(); 
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />);
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => { }
+  });
+  expect(wrapper.state('error')).toBe('');
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+});
+
+test('Should set new date on date change.', () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('Should set calendar focus on change', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused: true });
+  expect(wrapper.state('calendarFocused')).toBe(true);
+});
