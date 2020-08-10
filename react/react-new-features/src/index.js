a2ee0b196992a00df7b5b759f7572b56590656d5
Fremy Santana <frmysantana@yahoo.com>
2020-08-10 17:01:42 -0400

    finish react course updates

--- react/react-new-features/src/index.js
+++ react/react-new-features/src/index.js
@@ -1,56 +1,7 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-
-import notesReducer from './reducers/notes'
-import Note from './components/Note'
-
-const NoteApp = () => {
-  const [ notes, dispatch ] = useReducer(notesReducer, [])
-  const [ title, setTitle ] = useState('')
-  const [ body, setBody ] = useState('')
-
-  const addNote = (e) => {
-    e.preventDefault()
-    dispatch({type: 'ADD_NOTE', note: {title, body}})
-    setTitle('')
-    setBody('')
-  }
-
-  const removeNote = ({title}) => {
-    dispatch({ type: 'REMOVE_NOTE', title })
-  }
-
-  useEffect(() => {
-    let notes = localStorage.getItem('notes') ? 
-      JSON.parse(localStorage.getItem('notes')) :
-      []
-
-    dispatch({ type: 'POPULATE_NOTES', notes })
-  }, [])
-
-  useEffect(() => {
-    const updatedNotes = JSON.stringify(notes)
-    localStorage.setItem('notes', updatedNotes)
-  }, [ notes ])
-
-  return (
-    <div>
-      <h1>Notes</h1>
-
-      {notes.map(note => ( 
-        <Note key={note.title} note={note} removeNote={removeNote}/>
-      ))}
-
-      <p>Add Notes</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
-        <button>Add Note</button>
-      </form>
-    </div>
-  )
-}
+import NoteApp from './components/NoteApp'
 
 ReactDOM.render(
   <React.StrictMode>
--- react/react-new-features/src/reducers/notes.js
+++ react/react-new-features/src/reducers/notes.js
@@ -1,18 +1,16 @@
-// very similar to Redux, useReducer is for managing more complex state and abstracting the logic outside of the componenet itself to keep it clean and re-usable
-
-const notesReducer = ( state, action) => {
+const notesReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_NOTE':
-        return [
-            ...state, 
-            action.note
-        ]
+            return [
+                ...state,
+                action.note
+            ]
         case 'REMOVE_NOTE':
-        return state.filter(note => note.title !== action.title)
+            return state.filter(note => note.title !== action.title)
         case 'POPULATE_NOTES':
-        return action.notes
+            return action.notes
         default:
-        return state
+            return state
     }
 }
 
